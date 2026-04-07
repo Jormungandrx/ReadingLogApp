f1f35af56cce012443bec8b5da514d79bcab0783
Jormungandrx <171705931+Jormungandrx@users.noreply.github.com>
2026-04-07 16:41:14 -0600

    layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,12 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+   return (
+    <Tabs>
+      <Tabs.Screen name="index" options={{ title: "Home" }} />
+      <Tabs.Screen name="list" options={{ title: "My Logs" }} />
+      <Tabs.Screen name="form" options={{ title: "Add Logs" }} />
+      <Tabs.Screen name="calendar" options={{ title: "Calendar" }} />
+    </Tabs>
+  );
 }
