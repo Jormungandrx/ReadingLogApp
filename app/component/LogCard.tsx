bf551799e8e675de030729e043080e22e5ece047
Jormungandrx <171705931+Jormungandrx@users.noreply.github.com>
2026-04-07 16:18:03 -0600

    a glub glub glub

--- app/component/LogCard.tsx
+++ app/component/LogCard.tsx
@@ -10,9 +10,9 @@ interface BookCardProps {
 export default function BookCard({ book, onPress }: BookCardProps) {
   return (
     <TouchableOpacity style={styles.card} onPress={onPress}>
-      <Text style={styles.title}>{book.title}</Text>
-      <Text style={styles.author}>{book.author}</Text>
-      <Text style={styles.status}>{book.status}</Text>
+      <Text style={styles.title}> {book.title} </Text>
+      <Text style={styles.author}> {book.author} </Text>
+      <Text style={styles.status}> {book.status} </Text>
     </TouchableOpacity>
   );
 }
